6ba1f09f18544170b0c35d47c959aec0fe383b5d
Corrie <corrierthomas@gmail.com>
2021-02-26 16:49:51 -0500

    adds sql file

---
+++ EmployeeSql/sql.sql
@@ -0,0 +1,9 @@
+-- Delete these tables if they already exist
+DROP TABLE departments;
+DROP TABLE dept_emp;
+DROP TABLE dept_manager;
+DROP TABLE employees;
+DROP TABLE salaries;
+DROP TABLE titles;
+
+-- Create new tables
