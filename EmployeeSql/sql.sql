f530d55af85db0eddc673676747b0520f1e16966
Corrie <corrierthomas@gmail.com>
2021-02-26 17:03:10 -0500

    creates tables

--- EmployeeSql/sql.sql
+++ EmployeeSql/sql.sql
@@ -7,3 +7,37 @@ DROP TABLE salaries;
 DROP TABLE titles;
 
 -- Create new tables
+CREATE TABLE departments (
+    dept_no VARCHAR(30),
+    dept_name VARCHAR(30)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INT,
+    dept_no VARCHAR(30)
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR(30),
+    emp_no INT
+);
+
+CREATE TABLE employees (
+    emp_no INT,
+    emp_title_id VARCHAR(30),
+    birth_date DATE,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    sex VARCHAR(30)
+    hire_date DATE
+);
+
+CREATE TABLE salaries (
+    emp_no INT,
+    salary INT
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR(30),
+    title VARCHAR(30)
+);
