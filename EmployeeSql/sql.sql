0ccc18e9e4783341debe1eec0d02d5efa2c377c7
Corrie <corrierthomas@gmail.com>
2021-02-27 18:23:28 -0500

    answers q3

--- EmployeeSql/sql.sql
+++ EmployeeSql/sql.sql
@@ -51,21 +51,42 @@ CREATE TABLE salaries (
     salary INT
 );
 
--- 1. List employee number, last name, first name, sex, and salary
+-- 1. List the following details of each employee: 
+-- employee number, last name, first name, sex, and salary.
 SELECT employees.emp_no, employees.last_name, employees.first_name,
 employees.sex, salaries.salary
 FROM employees
 INNER JOIN salaries ON
 salaries.emp_no=employees.emp_no;
 
--- 2. List first name, last name, and hire date for employees who were hired in 1986
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date between '1986-01-01' and '1986-12-31'
 
--- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name
--- 4. List the department of each employee with the following information: employee number, last name, first name, and department name
--- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
--- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name
--- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
--- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+-- 3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name. 
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no,
+employees.first_name, employees.last_name
+FROM employees
+INNER JOIN dept_manager ON dept_manager.emp_no=employees.emp_no
+INNER JOIN departments ON departments.dept_no=dept_manager.dept_no;
+
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name. 
+
+
+-- 5. List first name, last name, and sex for employees 
+-- whose first name is "Hercules" and last names begin with "B."
+
+
+-- 6. List all employees in the Sales department, 
+-- including their employee number, last name, first name, and department name. 
+
+
+-- 7. List all employees in the Sales and Development departments, 
+-- including their employee number, last name, first name, and department name.
+
+
+-- 8. In descending order, list the frequency count of employee last names, 
+-- i.e., how many employees share each last name. 
