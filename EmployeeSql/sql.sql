81bebb99bb8528e5aad8be9cf006f256b44ded1e
Corrie <corrierthomas@gmail.com>
2021-02-27 18:32:35 -0500

    answers q5

--- EmployeeSql/sql.sql
+++ EmployeeSql/sql.sql
@@ -82,7 +82,10 @@ INNER JOIN departments ON departments.dept_no=dept_emp.dept_no;
 
 -- 5. List first name, last name, and sex for employees 
 -- whose first name is "Hercules" and last names begin with "B."
-
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name='Hercules' 
+    AND last_name between 'BA' and 'BZ'
 
 -- 6. List all employees in the Sales department, 
 -- including their employee number, last name, first name, and department name. 
