1e31defa035a9e5f125fbd0c2939b7e082032d9d
Corrie <corrierthomas@gmail.com>
2021-02-27 19:41:55 -0500

    answers q8

--- EmployeeSql/sql.sql
+++ EmployeeSql/sql.sql
@@ -107,3 +107,7 @@ WHERE dept_name='Sales' OR dept_name='Development';
 
 -- 8. In descending order, list the frequency count of employee last names, 
 -- i.e., how many employees share each last name. 
+SELECT employees.last_name, COUNT(employees.last_name) AS "Last Name Frequency"
+FROM employees
+GROUP BY employees.last_name
+ORDER BY "Last Name Frequency" DESC;
